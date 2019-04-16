a83e500af53f957d109103a8e0b75b3c507974d9
Ivan <officialaewnotik@gmail.com>
2019-04-16 16:05:02 +0700

    Updated to "function" files

---
+++ src/functions.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "func.h"
+
+float PLUS (float *A, int n)
+{
+	int i;
+	float value = 0;
+	for (i = 0; i < n; i++)
+	value += A[i];
+	return value;
+}
+
+float MINUS (float *A, int n)
+{
+	int i;
+	float value = A[0];
+	for (i = 1; i < n; i++)
+	value -= A[i];
+	return value;
+}
+
+float MULTIPLICATION (float *A, int n)
+{
+	int i;
+	float value = 1;
+	for (i = 0; i < n; i++)
+	value *= A[i];
+	return value;
+}
+
+float DIVISION (float a, float b, float *c)
+{
+	if (b == 0)
+		return -1;
+	else
+	{
+		*c = a / b;
+		return 0;
+	}
+}
